d5bda17027f7e4c4654050a854836a8c81c2d980
Musee Ullah <lae@lae.is>
2015-06-14 15:48:25 -0700

    actually create user home directory

--- chroot.sh
+++ chroot.sh
@@ -8,7 +8,7 @@ ln -s /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 
 mkinitcpio -p linux
 
-useradd -M -u 1017 -s /bin/bash -p '$6$L5Uyl3yxbWSiVbYi$uYmN04B.5sOSLcde105YQr.idicq.0ofHtofN5lJ/v0Zbyqe2IGRLo3HxSoSx0XgRq6dfPrFwPGulfCfXfRFC/' lae
+useradd -m -u 1017 -s /bin/bash -p '$6$L5Uyl3yxbWSiVbYi$uYmN04B.5sOSLcde105YQr.idicq.0ofHtofN5lJ/v0Zbyqe2IGRLo3HxSoSx0XgRq6dfPrFwPGulfCfXfRFC/' lae
 systemctl enable sshd.service
 
 su - lae -c "/bin/bash /chroot_as_lae.sh"
